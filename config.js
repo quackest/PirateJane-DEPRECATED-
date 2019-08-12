99f0a82f16196a46707150244d0fedfdd69360f5
WhiteOwll <48807848+WhiteOwll@users.noreply.github.com>
2019-08-11 18:54:37 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const config = {
+    //bot info
+    "token": 'Token here',
+    "steamAPI": "Steam API here for the verify command",
+    "prefix": '!',
+    "owner": 'Owner ID',
+    "logChannel": 'Log channel name (where the deleted/edited/etc messages go)',
+    "modLogs": 'Moderation log channel name (Where the mute/warn logs go)',
+
+    //database
+    "host": 'localhost',
+    "user": 'root',
+    "database": 'pj',
+    "password": 'toor',
+    
+
+        
+}
+module.exports = config;
